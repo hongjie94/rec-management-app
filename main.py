dc7467f0cfbd380aab923d30723226376e036aba
hongjie <hong.zhang94@gmail.com>
2023-05-01 13:55:53 -0400

    update

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ def server_error(e):
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    # app.run(host='127.0.0.1', port=8080, debug=True)
+     app.run()
 # [END gae_flex_quickstart]
 
 # Ensure responses aren't cached
